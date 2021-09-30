556e917d46ae1b6dd3358b98f86568973754b2d3
Seniv Pavlo <senivpavlo016@gmail.com>
2021-09-30 10:56:43 +0300

    Add corrected changes Laba1 and changes Laba1

--- Laba1/Laba1/Laba1.cpp
+++ Laba1/Laba1/Laba1.cpp
@@ -1,20 +1,162 @@
-﻿// Laba1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿//Task1
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <Windows.h>
 //
+//using namespace std;
+//
+//
+//void thread_for1() {
+//	cout << 1 << endl;
+//}
+//void thread_for2() {
+//	cout << 2 << endl;
+//}
+//
+//int main()
+//{
+//	thread t1(thread_for1);
+//	thread t2(thread_for2);
+//
+//	Sleep(1000);
+//
+//	return 0;
+//}
 
+//Task 2
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <Windows.h>
+//
+//using namespace std;
+//
+//
+//void thread_for1() {
+//	cout << 1 << endl;
+//}
+//void thread_for2() {
+//	cout << 2 << endl;
+//}
+//
+//int main()
+//{
+//	thread t1(thread_for1);
+//	thread t2(thread_for2);
+//
+//	Sleep(1000);
+//
+//	t1.detach();
+//	t2.detach();
+//
+//	system("pause");
+//	return 0;
+//}
+
+//Task 3
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <list>
+//#include <algorithm>
+//
+//
+//using namespace std;
+//
+//list<int> ls;
+//
+//
+//void AddToList(int number)
+//{
+//    for (int i = number; i < 10 + number; i++)
+//    {
+//        ls.push_back(i);
+//        cout << "Added: " << i << endl;
+//    }
+//}
+//
+//void ListContains(int number)
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        bool found = (find(ls.begin(), ls.end(), number) != ls.end());
+//
+//        if (found)
+//        {
+//            cout << "True " << endl;
+//        }
+//        else {
+//            cout << "False " << endl;
+//        }
+//    }
+//}
+//
+//int main() {
+//
+//    thread t1(AddToList, 12);
+//
+//    thread t2(ListContains, 18);
+//
+//    t1.join();
+//    t2.join();
+//
+//    system("pause");
+//    return 0;
+//}
+
+//Task 4
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <list>
+#include <algorithm>
 
-int main()
+
+using namespace std;
+
+mutex m1;
+
+list<int> ls;
+
+
+void AddToList(int number)
 {
-    std::cout << "Hello World!\n";
+    for (int i = number; i < 10 + number; i++)
+    {
+        m1.lock();
+        ls.push_back(i);
+        cout << "Added: " << i << endl;
+        m1.unlock();
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void ListContains(int number)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        m1.lock();
+        bool found = (find(ls.begin(), ls.end(), number) != ls.end());
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+        if (found)
+        {
+            cout << "True " << endl;
+        }
+        else {
+            cout << "False " << endl;
+        }
+        m1.unlock();
+    }
+}
+
+int main() {
+
+    thread t1(AddToList, 12);
+
+    thread t2(ListContains, 18);
+
+    t1.join();
+    t2.join();
+    system("pause");
+    return 0;
+}
