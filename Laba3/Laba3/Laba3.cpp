043d56db0eade036de27f67114d0d1ba1bc6097d
PavloSeniv <senivpavlo016@gmail.com>
2021-11-26 15:16:38 +0200

    Done 3 and 4 laba

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -1,20 +1,198 @@
-﻿// Laba3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿//Task1
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <queue>
+//#include <Windows.h>
 //
+//using namespace std;
+//
+//bool isEnd = false;
+//mutex mut;
+//queue<int> q;
+//
+//int primeNumber(unsigned long a, unsigned long i)
+//{
+//	return i * i <= a ? (a % i) && primeNumber(a, i + 1) : 1;
+//}
+//
+//void dataPreparation() {
+//	int count = 0;
+//	cout << "Enter the number of numbers: ";
+//	cin >> count;
+//	for (int i = 0; i < count; i++)
+//	{
+//		int value = 0;
+//		cout << "Enter number: ";
+//		cin >> value;
+//		q.push(value);
+//	}
+//	isEnd = true;
+//}
+//
+//void dataProcessing() {
+//	unique_lock<std::mutex> lk(mut);
+//	while (!isEnd) {
+//		lk.unlock();
+//		this_thread::sleep_for(chrono::milliseconds(100));
+//		lk.lock();
+//	}
+//	cout << "Processing" << endl;
+//	while (!q.empty()) {
+//		int v = q.front();
+//		q.pop();
+//		auto isPrime = primeNumber((long)v, (long)2);
+//		cout << "Number: " << v << " is prime =  " << (isPrime == 1 ? "True" : "False") << endl;
+//	}
+//}
+//
+//int main()
+//{
+//	thread DataPreparation(dataPreparation);
+//	thread DataProcessing(dataProcessing);
+//	DataPreparation.detach();
+//	DataProcessing.join();
+//}
 
+////Task2
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <queue>
+//#include <Windows.h>
+//#include <condition_variable>
+//
+//using namespace std;
+//
+//condition_variable dataCondition;
+//mutex mut;
+//int i = 0;
+//
+//void Awake() {
+//	this_thread::sleep_for(chrono::milliseconds(100));
+//	cout << "Informing" << endl;
+//	dataCondition.notify_all();
+//	this_thread::sleep_for(chrono::milliseconds(100));
+//	i = 1;
+//	cout << "Updated i=1. Informing" << endl;
+//	dataCondition.notify_all();
+//}
+//
+//void Waits() {
+//	unique_lock<mutex> lk(mut);
+//	cout << "Go to standby" << endl;
+//	dataCondition.wait(lk, [] {return i == 1; });
+//	cout << "The waiting state is over" << endl;
+//}
+//
+//int main() {
+//	thread waits1(Waits);
+//	thread waits2(Waits);
+//	thread waits3(Waits);
+//	thread awakeness(Awake);
+//	waits1.detach();
+//	waits2.detach();
+//	waits3.detach();
+//	awakeness.join();
+//	Sleep(1000);
+//}
+
+////Task3
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <queue>
+//#include <Windows.h>
+//#include <condition_variable>
+//
+//using namespace std;
+//condition_variable dataCondition;
+//mutex mut;
+//int i = 0;
+//
+//void Notify() {
+//	this_thread::sleep_for(chrono::milliseconds(200));
+//	cout << "Informing" << endl;
+//	dataCondition.notify_one();
+//	i = 1;
+//	cout << "Updated i=1. Informing" << endl;
+//	dataCondition.notify_one();
+//}
+//
+//void Waits(int n) {
+//	unique_lock<mutex> lk(mut);
+//	cout << "Go to standby from the stream: " << n << endl;
+//	dataCondition.wait(lk, [] {return i == 1; });
+//	cout << "Message from the thread: " << n << endl;
+//}
+//
+//int main() {
+//	thread waits1(Waits, 1);
+//	thread waits2(Waits, 2);
+//	thread waits3(Waits, 3);
+//	thread awakeness(Notify);
+//	waits1.detach();
+//	waits2.detach();
+//	waits3.detach();
+//	awakeness.join();
+//	Sleep(1000);
+//}
+
+//Task4
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <Windows.h>
+
+using namespace std;
+
+bool isEnd = false;
+mutex mut;
+condition_variable dataCondition;
+queue<int> q;
+
+int primeNumber(unsigned long a, unsigned long i)
+{
+	return i * i <= a ? (a % i) && primeNumber(a, i + 1) : 1;
+}
+
+void dataPreparation() {
+	cout << "False informing" << endl;
+	dataCondition.notify_all();
+	int count = 0;
+	cout << "Enter the number of numbers: ";
+	cin >> count;
+	for (int i = 0; i < count; i++)
+	{
+		int value = 0;
+		cout << "Enter number: ";
+		cin >> value;
+		q.push(value);
+	}
+	cout << "Correct notification" << endl;
+	dataCondition.notify_all();
+}
+
+void dataProcessing() {
+	unique_lock<std::mutex> lk(mut);
+	cout << "I started waiting" << endl;
+	dataCondition.wait(lk, [] {return !q.empty(); });
+	cout << "Processing started" << endl;
+	while (!q.empty()) {
+		int v = q.front();
+		q.pop();
+		auto isPrime = primeNumber((long)v, (long)2);
+		cout << "Number: " << v << " is prime =  " << (isPrime == 1 ? "True" : "False") << endl;
+	}
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	thread DataPreparation(dataPreparation);
+	thread DataProcessing(dataProcessing);
+	DataPreparation.detach();
+
+	DataProcessing.join();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Laba4/Laba4/Laba4.cpp
@@ -0,0 +1,226 @@
+﻿////Task1
+//#include <iostream>
+//#include <future>
+//#include <chrono>
+//
+//using namespace std;
+//
+//int getPrime(int n);
+//int root_number(int n);
+//
+//int main()
+//{
+//	int number = 1;
+//	cout << "Enter the number of prime numbers: ";
+//	cin >> number;
+//	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+//	future<int> result = async(launch::async, getPrime, number);
+//	cout << "Calculate root of " << number <<". Next enter 1" << endl;
+//	int choice = 0;
+//	cin >> choice;
+//	if (choice == 1)
+//	{
+//		cout << "Square of " << number << ": " << root_number(number) << endl;
+//	}
+//	int simple = result.get();
+//	cout << "Prime number is: " << simple << endl;
+//	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+//	std::cout << "The time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+//	std::cout << "The time difference = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[s]" << std::endl;
+//	system("pause");
+//}
+//
+//int root_number(int n) {
+//	return n * n;
+//}
+//
+//int getPrime(int n) {
+//	int count = 0;
+//	int max_limit = 100;
+//	if (max_limit < n)
+//	{
+//		max_limit = n;
+//	}
+//	int i = 1;
+//	while (count != n)
+//	{
+//		int kol = 0;
+//		for (int j = 1; j <= i; j++)
+//			if (i % j == 0) kol++;
+//		if (kol == 1 || kol == 2) {
+//			count++;
+//		}
+//		if (count == n)
+//		{
+//			return i;
+//		}
+//		i++;
+//	}
+//	return -1;
+//}
+
+
+//Task2
+//#include <iostream>
+//#include <future>
+//#include <chrono>
+//#include <deque>
+//#include <queue>
+//
+//using namespace std;
+//
+//int getPrime(int n);
+//int root_number(int n);
+//void task1();
+//void task2();
+//bool stop = false;
+//void dataProcessing();
+//
+//queue<packaged_task<int(int)>> qTask;
+//queue<int> qNumber;
+//
+//int main()
+//{
+//	task2();
+//	system("pause");
+//}
+//
+//void task2() {
+//	thread processing(dataProcessing);
+//	while (!stop)
+//	{
+//		int number = 1;
+//		cout << "Enter the number of prime numbers: ";
+//		cin >> number;
+//		if (number == -1)
+//			stop = true;
+//		packaged_task<int(int)> task(getPrime);
+//		qTask.push(move(task));
+//		qNumber.push(number);
+//	}
+//	processing.join();
+//}
+//
+//void dataProcessing() {
+//	while (!stop)
+//	{
+//		if (!qTask.empty())
+//		{
+//			packaged_task<int(int)> task = move(qTask.front());
+//			qTask.pop();
+//			int N = qNumber.front();
+//			qNumber.pop();
+//			task(N);
+//		}
+//	}
+//}
+//int getPrime(int n) {
+//	int count = 0;
+//	int max_limit = 100;
+//	if (max_limit < n)
+//	{
+//		max_limit = n;
+//	}
+//	int i = 1;
+//	while (count != n)
+//	{
+//		int kol = 0;
+//		for (int j = 1; j <= i; j++)
+//			if (i % j == 0) kol++;
+//		if (kol == 1 || kol == 2) {
+//			count++;
+//		}
+//		if (count == n)
+//		{
+//
+//			cout << "---Current number: " << n << " prime is " << i << endl;
+//			return i;
+//		}
+//		i++;
+//	}
+//	return -1;
+//}
+
+//Task3
+#include <iostream>
+#include <future>
+#include <chrono>
+#include <deque>
+#include <queue>
+
+using namespace std;
+int getPrime(int n);
+void task3();
+void currentOne(int N, promise<int> primePromise, promise<int> primePromise10, promise<bool> isReadyPromise) {
+	future<int> result = async(launch::async, getPrime, N);
+	int res = result.get();
+	move(primePromise).set_value(res);
+	move(isReadyPromise).set_value(true);
+	move(primePromise10).set_value(res * 10);
+
+}
+
+void currentTwo(future<bool> isReadyPromise, shared_future<int> primePromise) {
+	auto res = isReadyPromise.get();
+	if (res)
+	{
+		this_thread::sleep_for(2000ms);
+		auto n = primePromise.get();
+		cout << "Mul is " << n * n << endl;
+	}
+}
+
+int main()
+{
+	task3();
+	system("pause");
+}
+
+void task3() {
+	promise<int> ntePrime;
+	promise<int> nPrimex10;
+	promise<bool> isReady;
+	shared_future<int> primeFuture = ntePrime.get_future();
+	auto nPrime10Future = nPrimex10.get_future();
+	auto isReadyFuture = isReady.get_future();
+	int Nte = 1;
+	cout << "Enter the number of prime numbers: ";
+	cin >> Nte;
+	thread t1(currentOne, Nte, move(ntePrime), move(nPrimex10), move(isReady));
+	thread t2(currentTwo, move(isReadyFuture), primeFuture);
+	t1.detach();
+	t2.detach();
+	int nValue = primeFuture.get();
+	cout << "Prime number is " << nValue << endl;
+	int n10Value = nPrime10Future.get();
+	cout << "Number mul 10 is " << n10Value << endl;
+
+
+	system("pause");
+
+}
+int getPrime(int n) {
+	int count = 0;
+	int limit = 100;
+	if (limit < n)
+	{
+		limit = n;
+	}
+	int i = 1;
+	while (count != n)
+	{
+		int kol = 0;
+		for (int j = 1; j <= i; j++)
+			if (i % j == 0) kol++;
+		if (kol == 1 || kol == 2) {
+			count++;
+		}
+		if (count == n)
+		{
+			return i;
+		}
+		i++;
+	}
+	return -1;
+}
+
